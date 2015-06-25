da46e076b5a75683ec392327a1996217054ac828
bingo4508 <bingo4508@gmail.com>
2015-06-25 21:35:04 +0800

    revise dnn_recipe

--- dnn/dnn_recipe.sh
+++ dnn/dnn_recipe.sh
@@ -5,6 +5,7 @@
 # 4. Copy the script below the ========== symbol and run manually
 #
 
+pdnndir=$(pwd)/dnn/pdnn
 export PYTHONPATH=$PYTHONPATH:$(pwd)/dnn/pdnn
 export THEANO_FLAGS='mode=FAST_RUN,device=cpu,floatX=float32,openmp=true'
 
@@ -35,6 +36,24 @@ python script/preprocess.py $test $test_xwindow $window
 
 
 
+# Pre-training
+############################################################################################################
+result=MLDS-Final/model/mfcc_429
+train=MLDS-Final/mfcc/train_5window_merge.ark
+############################################################################################################
+
+python $pdnndir/cmds/run_RBM.py --train-data "$train,random=true" \
+                                --nnet-spec "819:1024:1024:48" \
+                                --wdir ./ --ptr-layer-number 2 \
+                            	--epoch-number 10 --batch-size 256 \
+                            	--learning-rate 0.08 --gbrbm-learning-rate 0.005 \
+                            	--momentum 0.5:0.9:5 --first_layer_type gb \
+                            	--param-output-file $result/rbm.mdl
+
+
+
+
+
 
 # Train
 #############################################################################################################
@@ -48,9 +67,8 @@ python $pdnndir/cmds/run_DNN.py --train-data "$train,random=true" \
                                 --valid-data "$valid" \
                                 --nnet-spec "819:1024:1024:48" --wdir ./ \
                                 --l2-reg 0.0001 --lrate "D:0.08:0.5:0.01,0.005:8" --model-save-step 1 \
-				--batch-size 256 \
-				--dropout-factor 0.5,0.5 --activation 3 \
-                                --param-output-file $result/dnn.param --cfg-output-file $result/dnn.cfg --kaldi-output-file $result/kaldi.model;
+				--batch-size 256 --ptr-file $result/rbm.mdl --input-dropout-factor 0.2\
+				--dropout-factor 0.5,0.5 --activation 3 --param-output-file $result/dnn.param --cfg-output-file $result/dnn.cfg --kaldi-output-file $result/kaldi.model;
 
 
 
@@ -61,6 +79,7 @@ python $pdnndir/cmds/run_DNN.py --train-data "$train,random=true" \
 #############################################################################################################
 result=MLDS-Final/model/mfcc_429
 test=MLDS-Final/mfcc/test_10window_hw1_merge.ark
+output=MLDS-Final/output
 #############################################################################################################
 
 python $pdnndir/cmds/run_Extract_Feats.py --data "$test" \
@@ -68,10 +87,10 @@ python $pdnndir/cmds/run_Extract_Feats.py --data "$test" \
 				--output-file "dnn.classify.pickle.gz" --layer-index -1 \
 				--batch-size 100
 
-python dnn/predict.py "dnn.classify.pickle.gz" $test > output/result
-paste -d',' script/static_data/test_hw1.name output/result > output/raw_result
-python script/map_phone_label.py output/raw_result output/final_result script/static_data/train_label.map script/static_data/48-39.map
-rm -f output/raw_result output/result
+python dnn/predict.py "dnn.classify.pickle.gz" $test > $output/result
+paste -d',' script/static_data/test_hw1.name $output/result > $output/raw_result
+python script/map_phone_label.py $output/raw_result $output/final_result script/static_data/train_label.map script/static_data/48-39.map
+rm -f $output/raw_result $output/result
 rm -f dnn.classify.pickle.gz
 
 
